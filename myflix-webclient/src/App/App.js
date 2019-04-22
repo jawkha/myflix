6be10676ce013a20b76b174de61015cd98690b64
Jawad Khawaja <devjawkha@gmail.com>
2019-04-22 23:57:24 +0500

    moved App.js and related files to a separate directory to reduce clutter in src

--- myflix-webclient/src/App/App.js
+++ myflix-webclient/src/App/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Router } from '@reach/router';
 
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import SearchResults from './components/SearchResults/SearchResults';
-import Favorites from './components/Favorites/Favorites';
-import MovieDetail from './components/MovieDetail/MovieDetail';
-import NotFound404 from './components/NotFound404/NotFound404';
-import Footer from './components/Footer/Footer';
+import Header from '../components/Header/Header';
+import Home from '../components/Home/Home';
+import SearchResults from '../components/SearchResults/SearchResults';
+import Favorites from '../components/Favorites/Favorites';
+import MovieDetail from '../components/MovieDetail/MovieDetail';
+import NotFound404 from '../components/NotFound404/NotFound404';
+import Footer from '../components/Footer/Footer';
 
 import './App.css';
 
